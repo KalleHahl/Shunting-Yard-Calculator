b1e4a41ea4d807f84b9c902e044a8ae62f0412a8
Kalle Hahl <kalle.hahl@hotmail.com>
2023-01-24 17:00:58 +0200

    a crude version of the algorithm which transforms from infix to postfx

---
+++ src/algorithms/postfix.py
@@ -0,0 +1,76 @@
+
+from collections import deque
+
+def to_postfix(string):
+    operators = '+-^*/()'
+    equation = deque(string)
+    operator_stack = deque()
+    the_stack = deque()
+
+    num = ''
+
+    while len(equation) != 0:
+
+        char = equation.popleft()
+        
+        if char.isdigit():
+            num += char
+
+        elif char in operators:
+            if len(num) != 0:
+                the_stack.append(num)
+                num = ''
+
+            if char in '+-':
+                if len(operator_stack) == 0:
+                    operator_stack.append(char)
+
+                elif operator_stack[-1] in '*/^':
+                    operator = operator_stack.pop()
+                    the_stack.append(operator)
+                    operator_stack.append(char)
+                else:
+                    operator_stack.append(char)
+            
+            elif char in '*/':
+                if len(operator_stack) == 0:
+                    operator_stack.append(char)
+                    continue
+                elif operator_stack[-1] in '+-':
+                    operator_stack.append(char)
+                    continue
+                elif operator_stack[-1] == '(':
+                    operator_stack.append(char)
+                    continue
+                operator = operator_stack.pop()
+                the_stack.append(operator)
+                operator_stack.append(char)
+                
+            elif char == '(':
+                operator_stack.append(char)
+            
+            elif char == ')':
+                while True:
+                    operator = operator_stack.pop()
+                    if operator == '(':
+                        break
+                    else:
+                        the_stack.append(operator)
+
+            elif char == '^':
+                operator_stack.append(char)
+        
+        if len(equation) == 0:
+            if len(num) != 0:
+                the_stack.append(num)
+            while len(operator_stack) != 0:
+                operator = operator_stack.pop()
+                the_stack.append(operator)
+        
+    return the_stack
+
+
+print(to_postfix('( 1 + 2 ) ^ ( 3 / 4 ) * ( 5 + 6 )'))
+print(to_postfix('5 + (1 + 2) * 4 - 3'))
+print(to_postfix('10^10'))
+    
