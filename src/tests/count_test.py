48e1e205c5ebaa08adeaed40c16162c16645d74c
Kalle Hahl <kalle.hahl@hotmail.com>
2023-02-01 14:22:59 +0200

    added a bunch of tests

--- src/tests/count_test.py
+++ src/tests/count_test.py
@@ -8,7 +8,7 @@ class Test_Count(unittest.TestCase):
     def setUp(self):
 
         self.test = count
-    
+
     # test correct value for addition
     def test_count_addition(self):
 
@@ -29,19 +29,31 @@ class Test_Count(unittest.TestCase):
         result = self.test(to_postfix('2+5*10-4/2'))
 
         self.assertEqual(result, 50)
-    
+
     # test correct value for power of
     def test_count_power_of(self):
 
         result = self.test(to_postfix('2+4*10+2^2^2+4/2'))
 
         self.assertEqual(result, 60)
-    
+
     # test correct value for equation with parentheses
     def test_count_parentheses(self):
 
         result = self.test(to_postfix('(1+2)*(10/2)^(2+1)'))
 
         self.assertEqual(result, 375)
+    
+    # test correct value for equation with float 
+    def test_count_float(self):
 
-print(count(to_postfix('5+5+5+5')))
+        result = self.test(to_postfix('1.2+4'))
+
+        self.assertEqual(result, 5.2)
+
+    # test correct value for equation with only division and multiplication
+    def test_div_mul_only(self):
+
+        result = self.test(to_postfix('2*8/4'))
+
+        self.assertEqual(result, 4)
--- src/tests/postfix_test.py
+++ src/tests/postfix_test.py
@@ -2,21 +2,22 @@ import unittest
 from src.algorithms.postfix import to_postfix
 from collections import deque
 
+
 class TestTo_postfix(unittest.TestCase):
 
     def setUp(self):
         self.test = to_postfix
-    
+
     # tests that the function appends correctly to the_stack by skipping spaces in input
     def test_skip_spaces(self):
-        
+
         result = self.test('4 + 2 + 4 + 5')
 
-        self.assertEqual(result, deque(['4','2','4','5','+','+','+']))
+        self.assertEqual(result, deque(['4', '2', '4', '5', '+', '+', '+']))
 
     # test for correct order of operators and operands in simple addition, if this works theres no reason to test subtraction since the are valued as equals in the algorithm
     def test_RPN__addition(self):
-        
+
         result = self.test('1+2+3+4')
 
         self.assertEqual(result, deque(['1', '2', '3', '4', '+', '+', '+']))
@@ -26,25 +27,39 @@ class TestTo_postfix(unittest.TestCase):
 
         result = self.test('2+5*10-4/2')
 
-        self.assertEqual(result, deque(['2','5','10','*','4','2','/','-','+']))
+        self.assertEqual(result, deque(
+            ['2', '5', '10', '*', '4', '2', '/', '-', '+']))
 
     # test for correct order when equation has power of
     def test_RPN_power_of(self):
-        
+
         result = self.test('2+4*10+2^2^2+4/2')
 
-        self.assertEqual(result, deque(['2', '4', '10', '*', '2', '2', '^', '2', '^', '4', '2', '/', '+', '+', '+']))
+        self.assertEqual(result, deque(
+            ['2', '4', '10', '*', '2', '2', '^', '2', '^', '4', '2', '/', '+', '+', '+']))
 
-    # test for correst order when equation has parentheses
+    # test for correct order when equation has parentheses
     def test_RPN_parentheses(self):
 
         result = self.test('(1+2)*(10/2)^(2+1)')
 
-        self.assertEqual(result, deque(['1', '2', '+', '10', '2', '/', '2', '1', '+', '^', '*']))
+        self.assertEqual(result, deque(
+            ['1', '2', '+', '10', '2', '/', '2', '1', '+', '^', '*']))
 
+    # test for correct order in ewuation with only division and multiplication
+    def test_RPN_div_mul(self):
 
+        result = self.test('5/5*5*5')
 
-
-
+        self.assertEqual(result, deque(
+            ['5', '5', '/', '5', '*', '5', '*']
+        ))
     
+    # test correct postfix for equation with float
+    def test_RPN_float(self):
 
+        result = self.test('1.2+4')
+
+        self.assertEqual(result, deque(
+            ['1.2','4','+']
+        ))
