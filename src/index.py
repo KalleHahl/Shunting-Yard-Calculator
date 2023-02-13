897800bdd157035d173fb3a97d4315c62a46123f
Kalle Hahl <kalle.hahl@hotmail.com>
2023-02-13 16:26:38 +0200

    changed index file and made variable saving possible

--- src/index.py
+++ src/index.py
@@ -1,15 +1,28 @@
 from src.algorithms.postfix import to_postfix
 from src.algorithms.count import count
+from src.variables import Variables
+
+variables = Variables()
 
 while True:
 
     try:
-        calc = input('\ncalculation: \n\n')
+        calculation = input('\ncalculation: \n\n')
     except:
         print(f"\nerror with ^, make sure to press twice and then space")
         continue
-    if calc == 'quit':
+
+    if calculation == 'quit':
         break
 
-    value = count(to_postfix(calc))
+    if '=' in calculation:
+        vars = calculation.split("=")
+        value = count(to_postfix(vars[-1]))
+        variables.add_variable(vars[0], str(value)) 
+        print(f"= {value}\n")
+        print('variable added!')
+        continue
+
+    check_variables = variables.fetch_variables(calculation)
+    value = count(to_postfix(check_variables))
     print(f"\n= {value}\n")
