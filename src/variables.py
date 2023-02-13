30aee01f6fcfcae0b8d3a10e47d67d31bed3060f
Kalle Hahl <kalle.hahl@hotmail.com>
2023-02-13 16:26:00 +0200

    Added class for variables

---
+++ src/variables.py
@@ -0,0 +1,17 @@
+
+class Variables:
+
+    def __init__(self):
+        self.vars = {}
+    
+    def add_variable(self, variable, value):
+        self.vars[variable] = value
+    
+    def fetch_variables(self, expression):
+
+        for keys, values in self.vars.items():
+            if keys in expression:
+                new = expression.split(keys)
+                expression = f"{new[0]}{values}{new[-1]}"
+        
+        return expression
