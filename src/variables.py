8322e419c240124df4f97595b7daf8416bb5ac92
Kalle Hahl <kalle.hahl@hotmail.com>
2023-02-15 01:15:04 +0200

    negative values are now stored inside parentheses in dictionary

--- src/variables.py
+++ src/variables.py
@@ -3,15 +3,16 @@ class Variables:
 
     def __init__(self):
         self.vars = {}
-    
+
     def add_variable(self, variable, value):
         self.vars[variable] = value
-    
+
     def fetch_variables(self, expression):
 
         for character in expression:
             if character in self.vars:
                 new = expression.split(character)
+                var = self.vars[character]
                 expression = f"{new[0]}{self.vars[character]}{new[-1]}"
-        
+
         return expression
